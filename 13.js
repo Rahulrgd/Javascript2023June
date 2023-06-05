54870a108fb3ad83cb4c35f2fed7b8f49feec04d
RahulGupta <rahulrgd21@gmail.com>
2023-06-05 23:18:15 +0530

    added 13 and 14.js

---
+++ 13.js
@@ -0,0 +1,16 @@
+// truthy and falsy values
+// falsy values:
+    // false
+    // null
+    // undefined
+    // 0
+    // ""
+
+let firstName = null;
+
+if(firstName)
+{
+    console.log(firstName);
+}else{
+    console.log("First name is empty");
+}
---
+++ 14.js
@@ -0,0 +1,8 @@
+// if else condition
+let age = 17;
+
+if(age<18){
+    console.log("User can't vote");
+}else{
+    console.log("User is allowed to vote");
+}
