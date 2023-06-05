1c3f9a80465e3fa68f6e128701f15dab79f09d4a
RahulGupta <rahulrgd21@gmail.com>
2023-06-05 22:39:53 +0530

    added 11.js

---
+++ 11.js
@@ -0,0 +1,12 @@
+// undefined 
+let firstName;
+console.log(firstName);
+
+// null
+let variable = null;
+console.log(variable);
+
+// binInt
+let int1 = BigInt(12);
+let int2 = 12n;
+console.log(int1, int2);
